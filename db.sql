d44c2bd9db9a0bfad62495f3d9e707f3cb68fb30
Ahmad Raju <rajuahmad6789@gmail.com>
2020-03-18 00:25:09 +0600

    reseller with db updated

--- db.sql
+++ db.sql
@@ -1,27 +1,35 @@
-/*
-SQLyog Enterprise - MySQL GUI v8.14 
-MySQL - 5.5.5-10.1.26-MariaDB : Database - login
-*********************************************************************
-*/
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 17, 2020 at 07:23 PM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
 
-/*!40101 SET NAMES utf8 */;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-/*!40101 SET SQL_MODE=''*/;
 
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`login` /*!40100 DEFAULT CHARACTER SET utf8 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-USE `login`;
+--
+-- Database: `login`
+--
 
-/*Table structure for table `bill_info` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `bill_info`;
+--
+-- Table structure for table `bill_info`
+--
 
 CREATE TABLE `bill_info` (
-  `bill_no` int(10) unsigned NOT NULL,
+  `bill_no` int(10) UNSIGNED NOT NULL,
   `c_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gross_total_price` int(11) NOT NULL,
   `paid_status` tinyint(1) NOT NULL DEFAULT '0',
@@ -29,112 +37,136 @@ CREATE TABLE `bill_info` (
   `customer_name` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `bill_info` */
+--
+-- Dumping data for table `bill_info`
+--
 
-insert  into `bill_info`(`bill_no`,`c_date`,`gross_total_price`,`paid_status`,`due_amount`,`customer_name`) values (101,'2019-11-23 00:00:00',11000,1,0,NULL),(102,'2019-11-27 12:10:19',233,0,2,NULL);
+INSERT INTO `bill_info` (`bill_no`, `c_date`, `gross_total_price`, `paid_status`, `due_amount`, `customer_name`) VALUES
+(101, '2019-11-23 00:00:00', 11000, 1, 0, NULL),
+(102, '2019-11-27 12:10:19', 233, 0, 2, NULL),
+(103, '2020-03-17 21:05:46', 22, 1, 0, 'aa');
 
-/*Table structure for table `bmw` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `bmw`;
+--
+-- Table structure for table `bmw`
+--
 
 CREATE TABLE `bmw` (
-  `b_no` int(10) unsigned NOT NULL,
+  `b_no` int(10) UNSIGNED NOT NULL,
   `stock_amount` bigint(20) NOT NULL,
   `amount_sold` bigint(20) DEFAULT '0',
   `price` int(11) DEFAULT '0',
   `total_price` bigint(20) DEFAULT '0',
   `date_added` datetime DEFAULT CURRENT_TIMESTAMP,
-  `stock_piece` int(11) DEFAULT '0',
-  PRIMARY KEY (`b_no`)
+  `stock_piece` int(11) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `bmw` */
+-- --------------------------------------------------------
 
-/*Table structure for table `cherry` */
-
-DROP TABLE IF EXISTS `cherry`;
+--
+-- Table structure for table `cherry`
+--
 
 CREATE TABLE `cherry` (
-  `b_no` int(10) unsigned NOT NULL,
+  `b_no` int(10) UNSIGNED NOT NULL,
   `stock_amount` bigint(20) NOT NULL,
   `amount_sold` bigint(20) DEFAULT '0',
   `price` int(11) DEFAULT '0',
   `total_price` bigint(20) DEFAULT '0',
   `date_added` datetime DEFAULT CURRENT_TIMESTAMP,
-  `stock_piece` int(11) DEFAULT '0',
-  PRIMARY KEY (`b_no`)
+  `stock_piece` int(11) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `cherry` */
+-- --------------------------------------------------------
 
-/*Table structure for table `customer` */
-
-DROP TABLE IF EXISTS `customer`;
+--
+-- Table structure for table `customer`
+--
 
 CREATE TABLE `customer` (
   `customer_name` varchar(50) DEFAULT 'unknown',
   `customer_id` int(11) NOT NULL,
   `dues` double DEFAULT NULL,
-  `date` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`customer_id`)
+  `date` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
 
-/*Data for the table `customer` */
+-- --------------------------------------------------------
 
-/*Table structure for table `diamond` */
-
-DROP TABLE IF EXISTS `diamond`;
+--
+-- Table structure for table `diamond`
+--
 
 CREATE TABLE `diamond` (
-  `b_no` int(10) unsigned NOT NULL,
+  `b_no` int(10) UNSIGNED NOT NULL,
   `stock_amount` bigint(20) NOT NULL,
   `amount_sold` bigint(20) DEFAULT '0',
   `price` int(11) DEFAULT '0',
   `total_price` bigint(20) DEFAULT '0',
   `date_added` datetime DEFAULT CURRENT_TIMESTAMP,
-  `stock_piece` int(11) DEFAULT '0',
-  PRIMARY KEY (`b_no`)
+  `stock_piece` int(11) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `diamond` */
+-- --------------------------------------------------------
 
-/*Table structure for table `jorjet` */
-
-DROP TABLE IF EXISTS `jorjet`;
+--
+-- Table structure for table `jorjet`
+--
 
 CREATE TABLE `jorjet` (
-  `b_no` int(10) unsigned NOT NULL,
+  `b_no` int(10) UNSIGNED NOT NULL,
   `stock_amount` bigint(20) NOT NULL,
   `amount_sold` bigint(20) DEFAULT '0',
   `price` int(11) DEFAULT '0',
   `total_price` bigint(20) DEFAULT '0',
   `date_added` datetime DEFAULT CURRENT_TIMESTAMP,
-  `stock_piece` int(11) DEFAULT '0',
-  PRIMARY KEY (`b_no`)
+  `stock_piece` int(11) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `jorjet` */
+-- --------------------------------------------------------
 
-/*Table structure for table `lx` */
-
-DROP TABLE IF EXISTS `lx`;
+--
+-- Table structure for table `lx`
+--
 
 CREATE TABLE `lx` (
-  `b_no` int(10) unsigned NOT NULL,
+  `b_no` int(10) UNSIGNED NOT NULL,
   `stock_amount` bigint(20) NOT NULL,
   `amount_sold` bigint(20) DEFAULT '0',
   `price` int(11) DEFAULT '0',
   `total_price` bigint(20) DEFAULT '0',
   `date_added` datetime DEFAULT CURRENT_TIMESTAMP,
-  `stock_piece` int(11) DEFAULT '0',
-  PRIMARY KEY (`b_no`)
+  `stock_piece` int(11) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `lx` */
+-- --------------------------------------------------------
 
-/*Table structure for table `user` */
+--
+-- Table structure for table `nidha`
+--
 
-DROP TABLE IF EXISTS `user`;
+CREATE TABLE `nidha` (
+  `b_no` int(10) UNSIGNED NOT NULL,
+  `stock_amount` bigint(20) NOT NULL,
+  `amount_sold` bigint(20) DEFAULT '0',
+  `price` int(11) DEFAULT '0',
+  `total_price` bigint(20) DEFAULT '0',
+  `date_added` datetime DEFAULT CURRENT_TIMESTAMP,
+  `stock_piece` int(11) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `nidha`
+--
+
+INSERT INTO `nidha` (`b_no`, `stock_amount`, `amount_sold`, `price`, `total_price`, `date_added`, `stock_piece`) VALUES
+(103, 0, 22, 1, 22, '2020-03-17 21:05:46', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
 
 CREATE TABLE `user` (
   `id` double DEFAULT NULL,
@@ -146,11 +178,65 @@ CREATE TABLE `user` (
   `date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `user` */
+--
+-- Dumping data for table `user`
+--
 
-insert  into `user`(`id`,`username`,`is_active`,`password`,`created_by`,`created_at`,`date`) values (101,'raju',1,'67959999','1','2019-11-07 11:25:44','2019-11-20'),(102,'aaaaa',0,'11111','1','2019-11-28 15:18:00','2019-11-14'),(103,'abc',1,'123','1','2019-11-26 11:20:54','2019-11-27'),(104,'bbb',0,'bbb','1','2019-11-05 11:26:23','2019-11-27'),(105,'raj',1,'123','1','2019-11-27 14:43:55','2019-11-27'),(106,'aaa',0,'111','1','2019-11-27 14:46:23','2019-11-27');
+INSERT INTO `user` (`id`, `username`, `is_active`, `password`, `created_by`, `created_at`, `date`) VALUES
+(101, 'raju', 1, '67959999', '1', '2019-11-07 05:25:44', '2019-11-20'),
+(102, 'aaaaa', 0, '11111', '1', '2019-11-28 09:18:00', '2019-11-14'),
+(103, 'abc', 1, '123', '1', '2019-11-26 05:20:54', '2019-11-27'),
+(104, 'bbb', 0, 'bbb', '1', '2019-11-05 05:26:23', '2019-11-27'),
+(105, 'raj', 1, '123', '1', '2019-11-27 08:43:55', '2019-11-27'),
+(106, 'aaa', 0, '111', '1', '2019-11-27 08:46:23', '2019-11-27');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bmw`
+--
+ALTER TABLE `bmw`
+  ADD PRIMARY KEY (`b_no`);
+
+--
+-- Indexes for table `cherry`
+--
+ALTER TABLE `cherry`
+  ADD PRIMARY KEY (`b_no`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `diamond`
+--
+ALTER TABLE `diamond`
+  ADD PRIMARY KEY (`b_no`);
+
+--
+-- Indexes for table `jorjet`
+--
+ALTER TABLE `jorjet`
+  ADD PRIMARY KEY (`b_no`);
+
+--
+-- Indexes for table `lx`
+--
+ALTER TABLE `lx`
+  ADD PRIMARY KEY (`b_no`);
+
+--
+-- Indexes for table `nidha`
+--
+ALTER TABLE `nidha`
+  ADD PRIMARY KEY (`b_no`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
